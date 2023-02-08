4d48f2d3b1b6427b1ac493c99a22738a3e2ae9db
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-02-07 21:28:08 -0800

    linked URLs to grab datas from geocoding and open weathermap. added temp wind and humidity for user's input city

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@
 var APIKey= "c6635ef8746ad380fe7694404b48b482";
 var geoURL= "http://api.openweathermap.org/data/2.5/weather?q=";
 var limit=1;
+var latitude = "";
+var longtitude ="";
 
 // variable for current date from day.dayjs
 
@@ -31,9 +33,48 @@ if (cityName === "") {
 // https://openweathermap.org/api/geocoding-api
 
 var geoURL="http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+fetch(geoURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        latitude =data[0].latitude;
+        longtitude=data[0].longtitude;
 
+ // https://openweathermap.org/api/one-call-3
+var weathermapURL= "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
+fetch(weathermapURL)
+        .then(function(response2) {
+            return response2.json();
+        })
+        .then(function (data2) {
+            console.log(data2);
+            const usercityInput = data2.name;
+            const usercitySearch=document.createElement("h2");
+            usercitySearch.textContent= usercityInput + " " + todayDate;
+            citySearch.append(usercitySearch);
 
-var weather=
-// https://openweathermap.org/api/one-call-3
+            const userIcon = data2.weather[0].icon;
+            const userWeatherIcon= document.createElement("img");
+            userWeatherIcon.setAttribute("src", "https://openweather.org/img/w/") + userIcon + ".png");
 
-"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+            citySearch.append(userWeatherIcon);
+
+            const userTemp = data2.main.temp;
+            const usernewTemp=document.createElement("p");
+            usernewTemp.textContent=
+            "Temperature: " + userTemp + "\u00B0";
+            tempSearch.append(usernewTemp);
+
+            const userWind=data2.wind.speed;
+            const usernewWind= document.createElement("p");
+            usernewWind.textContent="Wind:" + userWind + " MPH";
+            windSearch.append(usernewWind);
+
+            const userHumidity=data2.main.humidity+ "%";
+            const usernewHumidity=document.createElement("p");
+            usernewHumidity.textContent=
+            "Humidity: " + userHumidity;
+            humiditySearch.append(usernewHumidity);
+        
+            })
